c8bcc0a136fd99079d4fb2d2e4934d8c0f76c49a
chinmayanandamandula <chinmayanandamandula@gmail.com>
2024-11-11 11:36:00 +0530

    updated bycrypt

--- Backend/routes/adduser.js
+++ Backend/routes/adduser.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.post(
   "/",
