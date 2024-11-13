810cd5e41cfb963c3ff3e1b409592a3cb0079831
chinmayananda20 <128220072+chinmayananda20@users.noreply.github.com>
2024-11-13 13:57:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const cors = require('cors');
 app.use(cors({
-    origin:'https://dealsdry-77.vercel.app'   
+    origin:'*'   
 }));
 app.use(express.json());
 app.use(cors());
